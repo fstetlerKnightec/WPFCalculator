3b76539dc5c4234d687e1a26305840174e550cb6
frste <fredrik.stetler@knightec.se>
2024-02-07 14:45:47 +0100

    saved

--- WPFCalculatorSolution/WPFApplication/MainWindow.xaml.cs
+++ WPFCalculatorSolution/WPFApplication/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ namespace WPFApplication {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        string output = "";
+
         public MainWindow() {
             InitializeComponent();
         }
@@ -22,7 +25,58 @@ namespace WPFApplication {
         private void NumBtn_Click(object sender, RoutedEventArgs e) {
             string name = ((Button)sender).Name;
 
-            MessageBox.Show(name);
+            switch(name) {
+                case "OneBtn":
+                    output += "1";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "TwoBtn":
+                    output += "2";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "ThreeBtn":
+                    output += "3";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "FourBtn":
+                    output += "4";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "FiveBtn":
+                    output += "5";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "SixBtn":
+                    output += "6";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "SevenBtn":
+                    output += "7";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "EightBtn":
+                    output += "8";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "NineBtn":
+                    output += "9";
+                    OutputTextBlock.Text = output;
+                    break;
+
+                case "ZeroBtn":
+                    output += "0";
+                    OutputTextBlock.Text = output;
+                    break;
+            }
+
         }
     }
 }
