357c850168d292313b076588913c031d6048acf7
frste <fredrik.stetler@knightec.se>
2024-02-08 09:02:40 +0100

    moved logic to businessLogic

--- WPFCalculatorSolution/BusinessLogic/CalculatorLogic.cs
+++ WPFCalculatorSolution/BusinessLogic/CalculatorLogic.cs
@@ -1,9 +1,116 @@
 ﻿namespace BusinessLogic {
     public class CalculatorLogic {
 
-        public string MyCalculatorMethod() {
-            return "hey calculator";
+        string output = "";
+
+        double temp = 0;
+
+        string operation = "";
+
+        public void clear() {
+            output = "";
         }
 
+        public string getOutput() {
+            return output;
+        }
+
+        public void equal() {
+            double outputTemp;
+            switch (operation) {
+                case "Minus":
+                    outputTemp = temp - double.Parse(output);
+                    output = outputTemp.ToString();
+                    break;
+                case "Plus":
+                    outputTemp = temp + double.Parse(output);
+                    output = outputTemp.ToString();
+                    break;
+                case "Times":
+                    outputTemp = temp * double.Parse(output);
+                    output = outputTemp.ToString();
+                    break;
+                case "Divide":
+                    outputTemp = temp / double.Parse(output);
+                    output = outputTemp.ToString();
+                    break;
+            }
+        }
+
+        public void plus() {
+            if (output != "") {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Plus";
+            }
+        }
+
+        public void times() {
+            if (output != "") {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Times";
+            }
+        }
+
+        public void divide() {
+            if (output != "") {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Divide";
+            }
+        }
+
+        public void minus() {
+            if (output != "") {
+                temp = double.Parse(output);
+                output = "";
+                operation = "Minus";
+            }
+        }
+
+        public string addNumberToOutput(string name) {
+            switch (name) {
+                case "OneBtn":
+                    output += "1";
+                    break;
+                case "TwoBtn":
+                    output += "2";
+                    break;
+
+                case "ThreeBtn":
+                    output += "3";
+                    break;
+
+                case "FourBtn":
+                    output += "4";
+                    break;
+
+                case "FiveBtn":
+                    output += "5";
+                    break;
+
+                case "SixBtn":
+                    output += "6";
+                    break;
+
+                case "SevenBtn":
+                    output += "7";
+                    break;
+
+                case "EightBtn":
+                    output += "8";
+                    break;
+
+                case "NineBtn":
+                    output += "9";
+                    break;
+
+                case "ZeroBtn":
+                    output += "0";
+                    break;
+            }
+            return output;
+        }
     }
 }
--- WPFCalculatorSolution/WPFApplication/MainWindow.xaml.cs
+++ WPFCalculatorSolution/WPFApplication/MainWindow.xaml.cs
@@ -1,14 +1,6 @@
-﻿using System.Text;
+﻿using BusinessLogic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using BusinessLogic;
 
 namespace WPFApplication {
     /// <summary>
@@ -16,134 +8,40 @@ namespace WPFApplication {
     /// </summary>
     public partial class MainWindow : Window {
 
-        double temp = 0;
-
-        string output = "";
-
-        string operation = "";
+        CalculatorLogic calculator = new CalculatorLogic();
 
         public MainWindow() {
             InitializeComponent();
-           
-            this.DataContext = this;
         }
 
         private void NumBtn_Click(object sender, RoutedEventArgs e) {
             string name = ((Button)sender).Name;
-            switch(name) {
-                case "OneBtn":
-                    output += "1";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "TwoBtn":
-                    output += "2";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "ThreeBtn":
-                    output += "3";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "FourBtn":
-                    output += "4";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "FiveBtn":
-                    output += "5";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "SixBtn":
-                    output += "6";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "SevenBtn":
-                    output += "7";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "EightBtn":
-                    output += "8";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "NineBtn":
-                    output += "9";
-                    OutputTextBlock.Text = output;
-                    break;
-
-                case "ZeroBtn":
-                    output += "0";
-                    OutputTextBlock.Text = output;
-                    break;
-            }
+            OutputTextBlock.Text = calculator.addNumberToOutput(name);
         }
 
         private void EqualBtn_Click(object sender, RoutedEventArgs e) {
-            double outputTemp;
-            switch (operation) {
-                case "Minus":
-                    outputTemp = temp - double.Parse(output);
-                    output = outputTemp.ToString();
-                    OutputTextBlock.Text = output;
-                    break;
-                case "Plus":
-                    outputTemp = temp + double.Parse(output);
-                    output = outputTemp.ToString();
-                    OutputTextBlock.Text = output;
-                    break;
-                case "Times":
-                    outputTemp = temp * double.Parse(output);
-                    output = outputTemp.ToString();
-                    OutputTextBlock.Text = output;
-                    break;
-                case "Divide":
-                    outputTemp = temp / double.Parse(output);
-                    output = outputTemp.ToString();
-                    OutputTextBlock.Text = output;
-                    break;
-            }
+            calculator.equal();
         }
 
         private void PlusBtn_Click(object sender, RoutedEventArgs e) {
-            if (output != "") {
-                temp = double.Parse(output);
-                output = "";
-                operation = "Plus";
-            }
+            calculator.plus();
         }
 
         private void TimesBtn_Click(object sender, RoutedEventArgs e) {
-            if (output != "") {
-                temp = double.Parse(output);
-                output = "";
-                operation = "Times";
-            }
+            calculator.times();
         }
 
         private void DivideBtn_Click(object sender, RoutedEventArgs e) {
-            if (output != "") {
-                temp = double.Parse(output);
-                output = "";
-                operation = "Divide";
-            }
+            calculator.divide();
         }
 
         private void MinusBtn_Click(object sender, RoutedEventArgs e) {
-            if (output != "") {
-                temp = double.Parse(output);
-                output = "";
-                operation = "Minus";
-            }
+            calculator.minus();
         }
 
         private void ClearBtn_Click(object sender, RoutedEventArgs e) {
-            output = "";
-            OutputTextBlock.Text = output;
+            calculator.clear();
+            OutputTextBlock.Text = calculator.getOutput();
         }
     }
 }
